3c3efeff5dc015c4ab6a2570add1ba5e26e29428
iliapl <ilia.plyusch@yandex.ru>
2024-04-23 15:02:57 +0300

    abudabi?

---
+++ src/main/java/FileDo/CreateFile.java
@@ -0,0 +1,26 @@
+package FileDo;
+
+import util.EnvDoing;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CreateFile {
+    EnvDoing envDoing;
+    public CreateFile(EnvDoing envDoing){
+        this.envDoing = envDoing;
+    }
+            FileInputStream inputstream;
+
+    {
+        try {
+            inputstream = new FileInputStream(envDoing.getPATHcollection());
+            File operationFile = new File("C:\\Users\\plysc\\IdeaProjects\\file\\operationFile.xml");
+            if (){}
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
